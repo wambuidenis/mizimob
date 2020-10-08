b0f6559f5e9c67b439f2e713447ff38fadfd8615
simons22d <denniskiruku@gmail.com>
2020-10-08 11:36:59 +0300

    working UI

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # mizimob
-# mizimob
-# mizimob
--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import eventlet.wsgi
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("layout.html")
+
+
+@app.route('/item')
+def item():
+    return render_template("work-single.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run(port=8090)
+    port = 8090
+    eventlet.wsgi.server(eventlet.listen(('', port)), app)
