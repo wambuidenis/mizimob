d9c98ff412e02c24e9e9f580c719c5e7dbba8e04
simons22d <denniskiruku@gmail.com>
2020-10-21 16:51:22 +0300

    added default vals

--- mizimob/routes/routes.py
+++ mizimob/routes/routes.py
@@ -229,10 +229,10 @@ def add():
             description = form.description.data
             expires = form.expires.data
             # print(">>>>",title,category,price,description,expires)
-            active = True if form.active.data == "Active" else False
+            # active = True if form.active.data == "Active" else False
             # data
             # add to the db
-            lookup = Product(title, description, "Title", price, expires, active)
+            lookup = Product(title, description, "Title", price, expires, True)
             db.session.add(lookup)
             db.session.commit()
 
