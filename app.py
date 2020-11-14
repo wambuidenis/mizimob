f03d1b83316c87b86a224e8ef46af9ec79c29a8b
simons22d <denniskiruku@gmail.com>
2020-11-15 00:41:59 +0300

    added proper link

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import eventlet.wsgi
 
 
 if __name__ == '__main__':
-    app.run(host="localhost",port=7000,debug=True)
+    app.run(host="0.0.0.0",port=7000,debug=True)
     # port = 8090
     # eventlet.wsgi.server(eventlet.listen(('', port)), app)
