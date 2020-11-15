6a3c769f5235f0ecdfbaf1d89d4667df08aeecac
simons22d <denniskiruku@gmail.com>
2020-11-15 09:04:05 +0300

    eventlet

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import eventlet.wsgi
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=7000,debug=True)
-    # port = 8090
-    # eventlet.wsgi.server(eventlet.listen(('', port)), app)
+    # app.run(host="0.0.0.0",port=7000,debug=True)
+    port = 7000
+    eventlet.wsgi.server(eventlet.listen(('', port)), app)
